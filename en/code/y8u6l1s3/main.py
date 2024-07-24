d0f36408268cbeffdfc1333a47c25510980cde3b
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2024-07-24 13:01:51 +0100

    Update main.py
    updated code

--- en/code/y8u6l1s3/main.py
+++ en/code/y8u6l1s3/main.py
@@ -1,2 +1,6 @@
-user = "Claude"
-print("Hello", user)
+print("What is your name?")
+user = input()
+print("What is your lucky number?")
+lucky = input()
+print("Welcome", user)
+print("Your lucky number is", lucky)
