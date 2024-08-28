9c024c4de5bfe3e989455e2a046a6e54ebe92160
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2024-08-28 11:40:44 +0100

    updates

---
+++ en/code/8653a/main.py
@@ -0,0 +1,12 @@
+from random import randint
+while True: 
+  a = randint(2,12)
+  b = randint(2,12)
+  print(a, "times", b, "=")
+  answer = int(input())
+  product = a * b
+  if answer == product:
+  	    print("That is correct")
+  else:
+    print("I am sorry")
+    print(a, "times", b, "is", product)
--- en/code/y8u6l5s4/main.py
+++
@@ -1,7 +0,0 @@
-count = 10
-
-while count >= 1:
-  print(count)
-  count = count-1 
-
-print("Lift off!")
