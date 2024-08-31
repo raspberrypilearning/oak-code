62131dacc0ab8c53ac2016a17309532c8fdba787
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2024-08-31 08:57:02 +0100

    updates

---
+++ en/code/8661/main.py
@@ -0,0 +1,8 @@
+distance_traveled = 0
+treasure_found = False
+
+while treasure_found == False:
+print("Keep going", distance_traveled, "steps travelled")
+distance_traveled = distance_traveled + 10
+# Code to check for the treasure goes here
+print("Treasure found after", distance_traveled, "steps!")
