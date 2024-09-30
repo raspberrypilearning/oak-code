b550282164d8fe287494528b372cc8c1a50864eb
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2024-09-30 10:11:59 +0100

    updates

---
+++ en/code/8611a/main.py
@@ -0,0 +1,5 @@
+print("Once upon a time three bears lived in a little house.")
+print("There was a father bear, a mother bear and a baby bear.")
+print("One morning the bears made porridge for breakfast,")
+print("but the porridge was too hot to eat.")
+print("So the bears decided to take a walk to let it cool down.")
