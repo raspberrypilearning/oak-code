e98907588edcf5a9aefa465b2bbe346ce7cfbecf
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2024-10-09 10:20:45 +0100

    updates

--- en/code/8661/main.py
+++ en/code/8661/main.py
@@ -2,7 +2,9 @@ distance_traveled = 0
 treasure_found = False
 
 while treasure_found == False:
-    print("Keep going", distance_traveled, "steps travelled")
-    distance_traveled = distance_traveled + 10
+    print("Keep going", distance_traveled, "steps travelled so far")
+    print("How many steps do you want to go now?")
+    steps = int(input())
+    distance_traveled = distance_traveled + steps
 # Code to check for the treasure goes here
 print("Treasure found after", distance_traveled, "steps!")
--- en/code/8661a/main.py
+++ en/code/8661a/main.py
@@ -2,8 +2,11 @@ distance_traveled = 0
 treasure_found = False
 
 while treasure_found == False:
-  print("Keep going", distance_traveled, "steps travelled")
-  distance_traveled = distance_traveled + 10
-  if distance_traveled >= 250:
-    treasure_found = True
+    print("Keep going", distance_traveled, "steps travelled so far")
+    print("How many steps do you want to go now?")
+    steps = int(input())
+    distance_traveled = distance_traveled + steps
+# Code to check for the treasure goes here
+    if distance_traveled >= 250:
+      treasure_found = True
 print("Treasure found after", distance_traveled, "steps!")
