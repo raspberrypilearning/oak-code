86f7483682deb4a58a10b709d24454f3daa33ab4
Laura Sach <5183697+lawsie@users.noreply.github.com>
2024-12-19 13:14:08 +0000

    Add code examples

--- en/code/10671/main.py
+++ en/code/10671/main.py
@@ -1,1 +1,12 @@
-print("HI")
+import random
+
+print("Welcome to rock, paper, scissors")
+
+# Ask the player for their choice
+
+# The computer generates a random choice
+player2 = random.choice(["r", "p", "s"])
+print(f"The computer chose {player2}")
+
+# Use Boolean logic to compare the choices
+# and print the appropriate output
