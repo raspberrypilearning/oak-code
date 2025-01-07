4b7bce527d3f73369bd075dd0ad7bd74051ecb11
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2025-01-07 16:19:18 +0000

    updates

---
+++ en/code/9113a/main.py
@@ -0,0 +1,8 @@
+months = ["January", "February",
+          "March", "April", "May",
+          "June", "July", "August",
+          "September", "October", "November",
+          "December"]
+print("What month is it? (1-12)")
+month = int(input())
+print("It is", months[month-1])  # complete this line
