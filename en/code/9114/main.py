aa5c0c39842048c2d99d7afea2b2977057623ff0
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2025-01-07 17:02:37 +0000

    updates

--- en/code/9114/main.py
+++ en/code/9114/main.py
@@ -2,12 +2,11 @@ seasons = ["Winter", "Spring",
            "Summer", "Autumn"]
 print("What month is it? (1-12)")
 month = int(input())
-if month <= 2 or month == 12:
+if :
   season = 0
-elif month <= 5:
+elif:
   season = 1
-elif month <= 8:
+elif:
   season = 2
 else:
   season = 3
-print("It is", seasons[season])
