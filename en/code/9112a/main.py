f5ec4904a52fd01a1da1d59973cccc59e85c071a
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2025-01-07 16:15:37 +0000

    updates

---
+++ en/code/9112a/main.py
@@ -0,0 +1,9 @@
+months = ["January", "February",
+          "March", "April", "May",
+          "June", "July", "August",
+          "September", "October", "November",
+          "December"]
+print("These are the summer months:")
+print(months[6])  # complete this line
+print(months[7])  # complete this line
+print(months[8])  # complete this line
