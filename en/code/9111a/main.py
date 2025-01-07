27a4cfc2b2974022f666697fc6b1fba5aa9274b6
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2025-01-07 14:14:50 +0000

    updates

---
+++ en/code/9111a/main.py
@@ -0,0 +1,10 @@
+print("What day is it today?")
+day = int(input())
+if day <= 3:
+  print("It’s a weekday")
+  remaining = 
+  print(f"{remaining} days until the weekend")
+elif day == 4:
+  print("It’s Friday, just a day left until the weekend")
+else:
+  print("It’s the weekend!")
