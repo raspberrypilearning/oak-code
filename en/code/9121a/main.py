8a964d747cd7e65b9edadefad07ba6ca0fff2fc1
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2025-01-08 14:30:09 +0000

    updates

---
+++ en/code/9121a/main.py
@@ -0,0 +1,13 @@
+planets = ["Mercury", "Venus", 
+           "Earth", "Mars",
+           "Jupiter", "Saturn"]
+print("Planets in antiquity:")
+print(planets)
+planets.append("Uranus") # Add Uranus to the list
+planets.append("Neptune") # Add Neptune to the list
+planets.append("Pluto") # Add Pluto to the list
+print("Planets known by 1930:")
+print(planets)
+planets.remove("Pluto") # Remove Pluto from the list
+print("Planets after 2006:")
+print(planets)
