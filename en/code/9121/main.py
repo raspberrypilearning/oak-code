f481ba2324d5f93afdcf937b5017ce8c3783e859
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2025-01-08 14:15:50 +0000

    updates

--- en/code/9121/main.py
+++ en/code/9121/main.py
@@ -6,7 +6,7 @@ print(planets)
 # Add Uranus to the list
 # Add Neptune to the list
 # Add Pluto to the list
-print("Planets by 1930:")
+print("Planets known by 1930:")
 print(planets)
 # Remove Pluto from the list
 print("Planets after 2006:")
