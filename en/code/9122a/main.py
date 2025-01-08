eb8f6f74dd9d564f546ffd0169f998454b74e3c1
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2025-01-08 16:58:22 +0000

    updates

--- en/code/9122a/main.py
+++ en/code/9122a/main.py
@@ -4,13 +4,13 @@ defender_points = 0
 attacker = dicerolls(3)
 defender = dicerolls(2)
 print("Players’ rolls")
-print("Attacker:", attacker)
-print("Defender:", defender)
+print(f"Attacker: {attacker}")
+print(f"Defender: {defender}")
 attacker.sort(reverse=True)
 defender.sort(reverse=True)
 print("Sorted")
-print("Attacker:", attacker)
-print("Defender:", defender)
+print(f"Attacker: {attacker}")
+print(f"Defender: {defender}")
 if attacker[0] > defender[0]:
     defender_points = defender_points - 1
     attacker_points = attacker_points + 1
@@ -23,5 +23,5 @@ if attacker[1] > defender[1]:
 else:
     attacker_points = attacker_points - 1
     defender_points = defender_points + 1
-print("Attacker points:", attacker_points)
-print("Defender points:", defender_points)
+print(f"Attacker points: {attacker_points}")
+print(f"Defender points: {defender_points}")
