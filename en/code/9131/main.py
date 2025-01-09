602a6a0d4512873b03f0fe60ba1663e3aa1db865
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2025-01-09 17:46:47 +0000

    updates

--- en/code/9131/main.py
+++ en/code/9131/main.py
@@ -1,3 +1,3 @@
 from random import choice
 cities = ["Athens","Berlin","Dublin","London","Madrid","Paris","Prague","Stockholm","Vienna"]
-city = choice(cities)
+
--- en/code/9131a/main.py
+++ en/code/9131a/main.py
@@ -1,10 +1,11 @@
 from random import choice
 cities = ["Athens","Berlin","Dublin","London","Madrid","Paris","Prague","Stockholm","Vienna"]
-city = choice(cities)
+
 print("City hopping random planner")
 trip = []
 while len(trip) < 5:
+    city = choice(cities)
     trip.append(city)
-print("Itinerary:", trip)
+print(f"Itinerary: {trip}")
 
 
