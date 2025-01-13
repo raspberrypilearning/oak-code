280b54f27440e70108b6b1e82d311d2ded6bf9b1
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2025-01-13 19:41:42 +0000

    updates

--- en/code/9141/main.py
+++ en/code/9141/main.py
@@ -1,8 +1,5 @@
 from textfile import load
-heartbeat_data = load(1000)
-previous = -1.0
-for value in heartbeat_data:
-  if value > 0:
-    if previous < 0:
-      print("heartbeat detected")
-  previous = value
+rainfall_data = load("rainfall_data.txt")
+for value in rainfall_data:
+    print (value)
+
