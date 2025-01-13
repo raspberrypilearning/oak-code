3613d1eb00faf54f1e077bd32b0ac2f1303a080f
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2025-01-13 20:52:43 +0000

    updates

---
+++ en/code/9141a/main.py
@@ -0,0 +1,9 @@
+from textfile import load
+rainfall_data = load("rainfall_data.txt")
+count = 0
+for value in rainfall_data:
+    if value > 65:
+        count = count +1
+
+print (f"Number of times rainfall exceeds 65mm: {count}")
+
---
+++ en/code/9142a/main.py
@@ -0,0 +1,12 @@
+from textfile import load
+rainfall_data = load("rainfall_data.txt")
+max_rainfall = 0
+min_rainfall = 100
+for value in rainfall_data:
+    if value > max_rainfall:
+        max_rainfall = value
+    elif value < min_rainfall:
+        min_rainfall = value
+
+print (f"Maximum rainfall in mm: {max_rainfall}" )
+print (f"Minimum rainfall in mm: {min_rainfall}" )
