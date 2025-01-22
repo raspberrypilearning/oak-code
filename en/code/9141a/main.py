15571b1ae57e1bcfaf88c8a607fa65a7ba939cd9
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2025-01-22 10:40:33 +0000

    updates

--- en/code/9141a/main.py
+++ en/code/9141a/main.py
@@ -3,7 +3,7 @@ rainfall_data = load("rainfall_data.txt")
 count = 0
 for value in rainfall_data:
     if value > 65:
-        count = count +1
+        count = count + 1
 
 print (f"Number of times rainfall exceeds 65mm: {count}")
 
