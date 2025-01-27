b8d10bfa5b32465d11b645a2e8f27e1dbdeaf184
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2025-01-27 18:50:10 +0000

    updates

--- en/code/9151a/main.py
+++ en/code/9151a/main.py
@@ -1,4 +1,6 @@
-test_scores = [18,32,40,28,12,4,16,15,25,41,42,44,24,11,22,23,6,38,26,9,17,43,35,30,19]
+test_scores = [18,32,40,28,12,4,16,15,25,41,
+               42,44,24,11,22,23,6,38,26,9,
+               17,43,35,30,19]
 
 highest_score = max(test_scores)
 print(f"The highest score was {highest_score}")
