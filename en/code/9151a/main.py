8e8d39dc421f20c3afeb7c8c9de3874bccae4d91
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2025-01-27 18:46:58 +0000

    updates

---
+++ en/code/9151a/main.py
@@ -0,0 +1,12 @@
+test_scores = [18,32,40,28,12,4,16,15,25,41,42,44,24,11,22,23,6,38,26,9,17,43,35,30,19]
+
+highest_score = max(test_scores)
+print(f"The highest score was {highest_score}")
+lowest_score = min(test_scores)
+print(f"The highest score was {lowest_score}")
+total = 0
+for score in test_scores:
+    total = total + score
+average = total/len(test_scores)
+print(f"The average score was {average}")
+
