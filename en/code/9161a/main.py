8cdd902a75a4bc6763f03a7f0613899a780bc592
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2025-02-05 16:17:37 +0000

    updates

--- en/code/9161a/main.py
+++ en/code/9161a/main.py
@@ -1,21 +1,14 @@
-planted= [2.43, 0.82, 1.68, 1.50, 1.41, 
-          2.34, 2.05, 2.26, 3.13, 4.55]
+planted = [2.43, 0.82, 1.68, 1.50, 1.41, 
+           2.34, 2.05, 2.26, 3.13, 4.55]
 
 cut = [5.47, 3.69, 4.47, 4.29, 3.37,
        4.35, 2.86, 4.32, 5.52, 3.88]
 
-year = 2014
-net = 0 
-
 planted.append(3.03)
 cut.append(2.89)
 
-for position in range (0, len(planted)):
-    total = planted[position] - cut[position]
+for item in planted:
+    position = planted.index(item)
+    total = planted[position] -  cut[position]
     total = round(total, 2)
-    print(f"The net forest gain/loss for {year} was {total}")
-    position = position + 1
-    year = year + 1
-    net = net + total
-net = round(net,2)
-print(f"The total net forest gain/loss is {net} ")
+    print(f"The net forest gain/loss for year {position+1} was {total}")
