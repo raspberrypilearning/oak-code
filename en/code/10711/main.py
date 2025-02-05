816b0731c7650581fdcbd9e122a63eb8d63690ce
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2025-02-05 13:12:46 +0000

    updates

---
+++ en/code/10711/main.py
@@ -0,0 +1,28 @@
+def num_dogs():
+
+
+  return total_dogs
+
+def num_days():
+
+
+  return total_days
+
+def num_walks(total_dogs, total_days):
+
+
+  return total_walks
+
+def total_charge(total_walks):
+
+
+  return total_cost
+
+def invoice(total_dogs, total_days, total_walks, total_cost):
+
+
+total_dogs = num_dogs()
+total_days = num_days()
+total_walks = num_walks(total_dogs,total_days)
+total_cost = total_charge(total_walks)
+invoice(total_dogs, total_days, total_walks, total_cost)
