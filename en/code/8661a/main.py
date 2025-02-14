2c57d5df273f8046e893175fa4e2669aa76defbd
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2025-02-14 17:27:28 +0000

    updates

--- en/code/8661a/main.py
+++ en/code/8661a/main.py
@@ -5,7 +5,7 @@ while treasure_found == False:
     print(f"Keep going {distance_travelled} steps travelled so far")
     print("How many steps do you want to go now?")
     steps = int(input())
-    distance_travelled = distance_traveled + steps
+    distance_travelled = distance_travelled + steps
 # Code to check for the treasure goes here
     if distance_travelled >= 250:
       treasure_found = True
