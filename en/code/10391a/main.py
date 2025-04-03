78437f024dfdfc75c89dab01f5081464c5cf4c05
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2025-04-03 11:06:11 +0100

    updates

--- en/code/10391a/main.py
+++ en/code/10391a/main.py
@@ -14,12 +14,15 @@ print("6. Falafel            £2.90          £3.10")
 print("With crisps             +30p          +30p")
 print("With drink            £1.00          £1.00")
 print("-----------------------------------------------------------------")
+
+#take in users choices
 total = 0
 print("Would you like a sandwich or a wrap?: ")
 type = input()
 print("Which filling would you like (1-6)?: ")
 filling = int(input())
 
+#selection statements for sandwich options
 if type == "sandwich":
     if filling == 1:
         total = total + 2.8
@@ -27,34 +30,17 @@ if type == "sandwich":
         total = total + 2.5
     elif filling == 3 or filling == 4:
         total = total + 2.10
-    elif filling == 5 or filling == 6:
-        total  = total + 2.90
-    else:
-        print("You have not entered a valid filling option")	
-
-elif type == "wrap":
-    if filling == 1:
-        total = total + 3
-    elif filling == 2:
-        total = total + 2.7
-    elif filling == 3 or filling == 4:
-        total = total + 2.30
-    elif filling == 5 or filling == 6:
-        total  = total + 3.10
+    elif filling == 5:
+        total = total + 2.90
     else:
         print("You have not entered a valid filling option")
 
-print("Would you like crisps with your order? (y/n): ")
-crisps = input()
-if crisps == "y":
-    total = total + 0.30
+#selection statements for wrap options
 
-print("Would you like a drink with your order? (y/n): ")
-drink = input()
-if drink == "y":
-    total = total + 1
+
+#ask user if they want crisps and add to order total
+
+
+#ask user if they want a drink and add to order total 
 
 print(f"The total of your order is £{total}")
-
-if total > 4:
-    print("You have earned a sandwich stamp!")
