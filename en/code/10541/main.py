fe4d53ac603aead84a53c696dc5066a6c338cd2c
Matt Hogan <54578787+matthogan40@users.noreply.github.com>
2025-04-11 14:52:45 +0100

    renamned 10541 to 10543 and created 10541 new

--- en/code/10541/main.py
+++ en/code/10541/main.py
@@ -1,10 +1,10 @@
-days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-lesson_slots = ["9am - 10am", "10am - 11am", "11am - 12pm", "1pm - 2pm", "2pm - 3pm"]
-rooms = ["Room 101", "Room 102", "Room 103"]
+days_of_week = ["Monday", "Tuesday", "Wednesday", 
+                "Thursday", "Friday"]
+lesson_slots = ["9am - 10am", "10am - 11am", "11am - 12pm", 
+    "1pm - 2pm", "2pm - 3pm"]
 
 for day in days_of_week:
     print(f"Timetable for {day}:")
     for slot in lesson_slots:
-    	for room in rooms:
-            print(f"  {slot} - {room}")
+        print(f"  {slot}")
     print()
--- en/code/10542/main.py
+++ en/code/10542/main.py
@@ -1,8 +1,10 @@
-days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday",                 
-"Friday"]
-timeslots = ["9am - 11am", "11am - 1pm", "2pm - 4pm"]
+days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+lesson_slots = ["9am - 10am", "10am - 11am", "11am - 12pm", "1pm - 2pm", "2pm - 3pm"]
+rooms = ["Room 101", "Room 102", "Room 103"]
 
 for day in days_of_week:
-    print(f"Revision Timetable for {day}:")
-    for slot in timeslots:
-        print(f"  {slot}")
+    print(f"Timetable for {day}:")
+    for slot in lesson_slots:
+    	for room in rooms:
+            print(f"  {slot} - {room}")
+    print()
