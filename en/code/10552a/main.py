e9257a6fd82a12c828cd613e418d9bb888a91996
Matt Hogan <54578787+matthogan40@users.noreply.github.com>
2025-04-16 15:06:30 +0100

    Created 10552a

---
+++ en/code/10552a/main.py
@@ -0,0 +1,16 @@
+import random
+import string
+
+num_passwords = int(input("Enter the number of passwords to generate: "))
+password_length = 8
+characters = string.ascii_letters + string.digits
+
+print("\nGenerated passwords:")
+
+for password_num in range(num_passwords):
+    password = ""  
+    for _ in range(password_length):
+        random_char = random.choice(characters)
+        password += random_char
+
+    print(password)
