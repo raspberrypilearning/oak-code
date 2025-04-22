c721bda618bf9f093be1091efc1b41acda133bcd
Matt Hogan <54578787+matthogan40@users.noreply.github.com>
2025-04-22 09:55:32 +0100

    10551a updated

--- en/code/10551a/main.py
+++ en/code/10551a/main.py
@@ -1,11 +1,11 @@
-import random
-import time
+from random import randint
+from time import sleep
 
 user_number = int(input("Enter a number between 1 and 10: "))
 print(f"Your number is {user_number}")
 
 print("I am generating a random number...")
-time.sleep(2)
+sleep(2)
 
-random_number = random.randint(1, 10)
+random_number = randint(1, 10)
 print(f"My random number is: {random_number}")
