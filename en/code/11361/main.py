298fda73bd8d38b6b961bbcbcbca3083a0e95aad
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2025-08-11 15:45:23 +0100

    updates

---
+++ en/code/11361/main.py
@@ -0,0 +1,28 @@
+print ("Welcome to StreamScheduler")
+valid_duration = False
+
+while valid_duration == False:
+    stream_input = input("Enter the stream duration (20 to 40 minutes):")
+    if stream_input.isdigit():
+        stream_minutes = int(stream_input)
+        if stream_minutes < 20 or stream_minutes > 40:
+            print("Please enter a number between 20 and 40")
+        else:
+            valid_duration = True
+    else:
+        print("Please enter a numerical value")
+
+stream_seconds = stream_minutes * 60
+
+game_names = []
+game_durations = []
+total_time = 0
+game_name = input("Enter game name: ")
+games_names.append(game_name)
+review_duration = input("Enter review duration in seconds: ")
+review_seconds = int(review_duration)
+game_durations.append(review_seconds)
+print(game_names)
+print(game_durations)
+
+total_time = total_time + review_seconds
