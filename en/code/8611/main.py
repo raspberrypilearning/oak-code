08e50f83c739123f6a33db605067e513e8d7aff5
OakGenGit <gengit@thenational.academy>
2025-08-21 13:00:58 +0000

    fix: add comment to 8611

--- en/code/8611/main.py
+++ en/code/8611/main.py
@@ -3,3 +3,4 @@ Print("There was a father bear, a mother bear and a baby bear.")
 print("One morning the bears made porridge for breakfast,")
 print("but the porridge was too hot to eat."
 print So the bears decided to take a walk to let it cool down.")
+# Using print(), continue the story below
